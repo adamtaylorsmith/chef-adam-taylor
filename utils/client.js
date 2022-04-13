6c5afc5ac8e621a98b0f3230f99a84d3bd267ccc
adamtaylorsmith <adamtaylor@icloud.com>
2022-04-12 22:49:59 -0400

    client.js

--- utils/client.js
+++ utils/client.js
@@ -3,6 +3,7 @@ import config from './config';
 const client = sanityClient({
   projectId: config.projectId,
   dataset: config.dataset,
+  apiVersion: '2021-08-31',
   useCdn: true,
 });
 export default client;
