e201f350b1d5491482a6d44ed868df4993943549
adamtaylorsmith <adamtaylor@icloud.com>
2022-04-14 13:27:34 -0400

    images

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,47 @@
 import Layout from '../components/Layout'
-import { Grid, Container, Typography, Link} from '@mui/material'
+import { Grid, Container, Typography } from '@mui/material'
 // import { useState } from 'react';
 import classes from '../utils/classes'
-import NextLink from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
 
-    function changeBackground(e) {
-        e.target.style.color = 'blue';
-      }
-
     return (
         <>
-
             <Layout>
                 <Container>
                     <Grid container>  
-                        <Grid item xs={12}>
+                        <Grid item xs={12} md={4}>
                             <Typography sx={classes.homePage}>
                                 Chef Adam&#39;s Healthy Cuisine
                             </Typography> 
+                        </Grid> 
+                        <Grid item xs={12} md={8}>
+                            <Image src="/adam-hitwgc.png" alt="Adam during an Iron Chef event at Hole in the Wall Gang Camp in Ashford, CT" /> 
                         </Grid>
+                        {/* https://cdn.sanity.io/images/yx8hhiz1/production/faf3b38d7810fda041363145be384a3022fc5af2-1375x1750.png */}
                     </Grid>
                 </Container>
                 <Container>
                     <Typography sx={classes.homePageContent}>
-                        <NextLink passHref href="https://cafeflora.com/" >
-                            <a rel="noopener noreferrer" target="_blank" sx={classes.outsideLink}>Cafe Flora</a> 
-                        </NextLink> in Seattle, WA<br/>
-                        <NextLink href="https://www.holeinthewallgang.org/" passHref rel="noreferrer" target="_blank">
-                            <a onMouseOver={changeBackground}>The Hole in the Wall Gang Camp</a> 
-                        </NextLink> in Ashford, CT <br/>
-                        <NextLink href="https://www.upress.umn.edu/book-division/books/the-cafe-brenda-cookbook" passHref rel="noreferrer" target="_blank">
-                            <Link>Cafe Brenda</Link> 
-                        </NextLink> in Minneapolis, MN<br/>
-                        <NextLink href="https://www.ibj.com/articles/56946-mass-ave-farm-to-table-pioneer-r-bistro-to-close" passHref rel="noreferrer" target="_blank">
-                            <Link>R Bistro</Link> 
-                        </NextLink> in Indianapolis, IN<br/>
-                        <NextLink href="https://greensrestaurant.com/" passHref rel="noreferrer" target="_blank">
-                            <Link>Greens</Link> 
-                        </NextLink> in San Francisco, CA<br/>
-                        Culinary Arts at the <NextLink href="https://www.ngihca.edu/" passHref rel="noreferrer" target="_blank">
-                            <Link>Natural Gourmet Institute</Link> 
-                        </NextLink> in New York, NY<br/>
+                        <a target="_blank" href="https://cafeflora.com/" rel="noopener noreferrer" style={{color: '#fcb762'}}>
+                        Cafe Flora
+                        </a> in Seattle, WA<br/>
+                        <a target="_blank" href="https://www.holeinthewallgang.org/" rel="noopener noreferrer" style={{color: '#fcb762'}}>
+                        Hole in the Wall Gang Camp
+                        </a> in Ashford, CT <br/>
+                        <a target="_blank" href="https://www.upress.umn.edu/book-division/books/the-cafe-brenda-cookbook" rel="noopener noreferrer" style={{color: '#fcb762'}}>
+                        Cafe Brenda
+                        </a> in Minneapolis, MN<br/>
+                        <a target="_blank" href="https://www.ibj.com/articles/56946-mass-ave-farm-to-table-pioneer-r-bistro-to-close" rel="noopener noreferrer" style={{color: '#fcb762'}}>
+                        R Bistro
+                        </a> in Indianapolis, IN<br/>
+                        <a target="_blank" href="https://greensrestaurant.com/" rel="noopener noreferrer" style={{color: '#fcb762'}}>
+                        Greens
+                        </a> in San Francisco, CA<br/>
+                        <a target="_blank" href="https://www.ngihca.edu/" rel="noopener noreferrer" style={{color: '#fcb762'}}>
+                        Natural Gourmet Institute
+                        </a> in New York, NY<br/>
                     </Typography> 
                 </Container>
             </Layout>; 
